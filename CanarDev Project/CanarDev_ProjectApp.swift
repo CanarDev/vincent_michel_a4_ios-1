3a63923d1d25721e62a4b119ad8249405282e2b8
CanarDev <vincentmichelnavarro@gmail.com>
2023-11-20 11:27:06 +0100

    Initial Commit

---
+++ CanarDev Project/CanarDev_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanarDev_ProjectApp.swift
+//  CanarDev Project
+//
+//  Created by Vincent Michel  on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CanarDev_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
